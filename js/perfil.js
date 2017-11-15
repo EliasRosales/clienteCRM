92f44f551a8946f9d7d4883ecc026eab7d3503a0
Elias Rosales <mansion1@hotmail.com>
2017-11-15 10:27:41 -0600

    productos

--- js/perfil.js
+++ js/perfil.js
@@ -7,52 +7,30 @@ $(document).ready(function() {
         $(".menuusertxt").text(localStorage.getItem("Name") + " " + localStorage.getItem("LastName"));
         $(".username").text(localStorage.getItem("Nickname"));
 
-        //Seccion que trae los productos
-        var urlGetProduct = url + "purchases/get_p/";
-        $.get(urlGetProduct, function (response) {
-            for (var i = 0; i < (response.data).length; i++) {
-                console.log(response.data[i]);
-                $(".container").append('<article>' + '<div class="comprar">' + '<p>Descripción</p>' + '<p>' + (response.data[i]).description + '</p>' + '<div class="confCopmra" hidden >' + '<p> Comprar</p>' + '<p> Precio: $' + (response.data[i]).price + ' MX</p>' + '<p>Disponibles:' + (response.data[i]).stock + ' </p>' + '<p>Cantidad: <input class="prod_cantidad" align="middle" type="numeric" name="cantidad" value="1"></p><p>Total:' + (response.data[i]).price * $(".prod_cantidad").val() + '</p>' + '<p id="boton" onclick="confirmar()">Confirmar</p>' + '</div>' + '</div>' + '<h1>' + (response.data[i]).name + '</h1>' + '</article><style type="text/css">' + '.container article {' + 'background-image: url(' + (response.data[i]).image + ');}' + '</style>');
+        var idClient = event.target.id;
+        completeURL = url + "users/view_profile/"
+        $.post(completeURL, {user_id: idClient} , function(response) {
+            if(response.response == 1){
+                $("#upClientName").attr("placeholder", response.user.name);
+                $("#upClientLastName").attr("placeholder", response.user.last_name);
+                $("#upClientUser").attr("placeholder", response.user.nickname);
+                $("#upClientMail").attr("placeholder", response.user.email);
+                $("#clientSendUpdate").attr("id", idClient);
+                $("#updateClientForm").slideDown('400');
+
+            }else{
+                swal(
+                    'Error!',
+                    'Algo ocurrio mal.',
+                    'error'
+                )
             }
-
-            $(".rateyo").rateYo({
-
-                rating: 0,
-                numStars: 5,
-                precision: 0,
-                minValue: 1,
-                maxValue: 5
-            }).on("rateyo.change", function (e, data) {
-
-                $("#calificacion").text(data.rating);
-            });
-
         });
     }else{
         window.location.href = "login.html";
     }
 });
-function showClientUpdate(event){
-    var idClient = event.target.id;
-    completeURL = url + "users/view_profile/"
-    $.post(completeURL, {user_id: idClient} , function(response) {
-        if(response.response == 1){
-            $("#upClientName").attr("placeholder", response.user.name);
-            $("#upClientLastName").attr("placeholder", response.user.last_name);
-            $("#upClientUser").attr("placeholder", response.user.nickname);
-            $("#upClientMail").attr("placeholder", response.user.email);
-            $("#clientSendUpdate").attr("id", idClient);
-            $("#updateClientForm").slideDown('400');
 
-        }else{
-            swal(
-                'Error!',
-                'Algo ocurrio mal.',
-                'error'
-            )
-        }
-    });
-}
 function updateClient(event){
     var urlGetClient = url + "users/get_users_by_type/";
     var idClient = event.target.id;
