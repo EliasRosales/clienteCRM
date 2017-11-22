beb419283c12f014397411cdff6c4e15c49ebf47
theprap <rikymon2@hotmail.com>
2017-11-22 12:35:28 -0600

    correcciones

--- js/index.js
+++ js/index.js
@@ -12,8 +12,8 @@ $(document).ready(function() {
         var urlGetProduct = url + "products/get_products/";
         $.get(urlGetProduct, function (response) {
             for (var i = 0; i < (response.data).length; i++) {
-                console.log(response.data[i]);
-                $(".container").append('<article>' + '<div class="comprar">' + '<p>Descripción</p>' + '<p>' + (response.data[i]).description + '</p>' + '<div class="confCopmra" hidden >' + '<p> Comprar</p>' + '<p id="product_price"> Precio(mx/100): $</p><input id="price" type="number" value="' + (response.data[i]).price + '" disabled>' + '<p id="product_stock">Disponibles: </p> <input id="stock" type="number" value="' + (response.data[i]).stock + '" disabled> ' + '<p id="product_quantity">Cantidad: </p><input onchange="calTotal(event);" id="quantity"  type="number" min="1" max="' + (response.data[i]).stock + '"><p id="product_total"> Total: $ </p><input id="total" type="number"  type="number" disabled>' + '<p class="button" id="'+(response.data[i]).id+'"  onclick="confirmar(event)">Confirmar</p>' + '</div>' + '</div>' + '<h1>' + (response.data[i]).name + '</h1>' + '</article><style type="text/css">' + '.container article {' + 'background-image: url(' + (response.data[i]).image + ');}' + '</style>');
+                // console.log(response.data[i]);
+                $(".container").append('<article style="background-image: url(' + response.data[i].image + ')">' + '<div class="comprar">' + '<p>Descripción</p>' + '<p>' + response.data[i].description + '</p>' + '<div class="confCopmra" hidden >' + '<p> Comprar</p>' + '<p id="product_price"> Precio(mx/100): $</p><input id="price" type="number" value="' + (response.data[i]).price + '" disabled>' + '<p id="product_stock">Disponibles: </p> <input id="stock" type="number" value="' + (response.data[i]).stock + '" disabled> ' + '<p id="product_quantity">Cantidad: </p><input onchange="calTotal(event);" id="quantity"  type="number" min="1" max="' + (response.data[i]).stock + '"><p id="product_total"> Total: $ </p><input id="total" type="number"  type="number" disabled>' + '<p class="button" id="'+(response.data[i]).id+'"  onclick="confirmar(event)">Confirmar</p>' + '</div>' + '</div>' + '<h1>' + (response.data[i]).name + '</h1>' + '</article>');
             }
             $("section article .comprar p:first-child").on("click", function () {
                 $(this).parent().toggleClass("animar");
@@ -37,31 +37,34 @@ $(document).ready(function() {
     }
 });
 
-function calTotal(e){
-    var cantidad = $(this).val();
-    var precio = $("#price").val();
-    $("#total").val(cantidad * precio );
-    console.log(cantidad * precio);
+function calTotal(event){
+    var cantidad = event.path[0].value;
+    var precio = event.path[1].children[2].value;
+    total = cantidad * precio
+    event.path[1].children[8].value = total;
+    // console.log(cantidad * precio);
 }
 
 function confirmar(event){
     var urlPurchase = url + "purchases/register_purchase/";
     var clientid = localStorage.getItem("Id");
     var productid = event.target.id;
-    var quantity = $("#quantity").val();
-    var total = $("#total").val();
+    var quantity = event.path[1].children[6].value;
+    var total = event.path[1].children[8].value;
 
     $.post(urlPurchase, {user_id: clientid, product_id: productid, quantity: quantity, total: total} , function(response) {
+        console.log('hola');
+        console.log(response);
         if(response.response == 1){
             swal(
                 'Success',
-                'Usuario agregado correctamente',
+                'Se realizó correctamente la compra',
                 'success'
             )
         }else{
             swal(
                 'Error',
-                'Error Ingresando el Usuario',
+                'Error intente nuevamente',
                 'error'
             )
         }
